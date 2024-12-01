5aa83e1cd3101a904972434a3f8b25b2f710111d
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2024-12-01 10:46:11 +0000

    Cleaned up

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,23 +1,25 @@
 advent_of_code::solution!(1);
 
 use std::collections::HashMap;
-use itertools::Itertools;
 
 pub fn get_lists(input: &str) -> (Vec<i32>, Vec<i32>) {
-    let col1 = input.lines().map(|l| {
-        l.split_whitespace().nth(0).map(|l| l.parse::<i32>().unwrap()).unwrap()
-    }).sorted().collect::<Vec<_>>();
+    let mut col1 = Vec::new();
+    let mut col2 = Vec::new();
 
-    let col2 = input.lines().map(|l| {
-        l.split_whitespace().nth(1).map(|l| l.parse::<i32>().unwrap()).unwrap()
-    }).sorted().collect::<Vec<_>>();
+    for l in input.lines() {
+        let (a, b) = l.split_once("   ").unwrap();
+        col1.push(a.parse().unwrap());
+        col2.push(b.parse().unwrap());
+    }
 
     (col1, col2)
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
 
-    let (col1, col2) = get_lists(input);
+    let (mut col1, mut col2) = get_lists(input);
+    col1.sort_unstable();
+    col2.sort_unstable();
     let ret = col1.iter().zip(col2.iter()).map(|(&a, &b)| {
         (a - b).abs() as u32
     }).sum();
@@ -40,19 +42,3 @@ pub fn part_two(input: &str) -> Option<u32> {
     Some(ret as u32)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-}
