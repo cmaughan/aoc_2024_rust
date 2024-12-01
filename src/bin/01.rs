5f662823a0ec575d8365e35a5474790f2a097677
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2024-12-01 10:19:48 +0000

    Finished

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,43 @@
 advent_of_code::solution!(1);
 
+use std::collections::HashMap;
+use itertools::Itertools;
+
+pub fn get_lists(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let col1 = input.lines().map(|l| {
+        l.split_whitespace().nth(0).map(|l| l.parse::<i32>().unwrap()).unwrap()
+    }).sorted().collect::<Vec<_>>();
+
+    let col2 = input.lines().map(|l| {
+        l.split_whitespace().nth(1).map(|l| l.parse::<i32>().unwrap()).unwrap()
+    }).sorted().collect::<Vec<_>>();
+
+    (col1, col2)
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+
+    let (col1, col2) = get_lists(input);
+    let ret = col1.iter().zip(col2.iter()).map(|(&a, &b)| {
+        (a - b).abs() as u32
+    }).sum();
+
+    Some(ret)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(23)
+    let (col1, col2) = get_lists(input);
+
+    let mut counts = HashMap::new();
+    for v in col2 {
+        *counts.entry(v).or_insert(0) += 1;
+    }
+
+    let ret = col1.iter().map(|a| {
+        counts.get(a).unwrap_or(&0) * a
+    }).sum::<i32>();
+
+    Some(ret as u32)
 }
 
 #[cfg(test)]
