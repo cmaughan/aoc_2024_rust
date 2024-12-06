6622beab721a2cec03189e00ad1d739d8b968e25
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2024-12-06 12:28:06 +0000

    Part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,15 +1,18 @@
 use std::collections::HashSet;
+use std::hash::Hash;
+use itertools::Itertools;
 
 advent_of_code::solution!(6);
 
+#[derive(Eq, PartialEq, Hash, Copy, Clone)]
 enum Dir {
     Up,
     Down,
     Left,
-    Right
+    Right,
 }
 
-fn move_pos(pos: (i32, i32), direction : &Dir) -> (i32, i32) {
+fn move_pos(pos: (i32, i32), direction: &Dir) -> (i32, i32) {
     match direction
     {
         Dir::Up => (pos.0 - 1, pos.1),
@@ -19,7 +22,7 @@ fn move_pos(pos: (i32, i32), direction : &Dir) -> (i32, i32) {
     }
 }
 
-fn next_dir(direction : &Dir) -> Dir {
+fn next_dir(direction: &Dir) -> Dir {
     match direction
     {
         Dir::Up => Dir::Right,
@@ -29,7 +32,15 @@ fn next_dir(direction : &Dir) -> Dir {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+struct Input
+{
+    guard: (i32, i32),
+    width: i32,
+    height: i32,
+    data: Vec<Vec<u8>>,
+}
+
+fn parse(input: &str) -> Input {
     let lines: Vec<Vec<u8>> = input
         .lines()
         .map(|line| line.as_bytes().to_vec())
@@ -38,23 +49,28 @@ pub fn part_one(input: &str) -> Option<u32> {
     let height = lines.len() as i32;
     let width = lines[0].len() as i32;
 
-    let mut guard = lines.iter().enumerate().find_map(|(y, line)| {
+    let guard = lines.iter().enumerate().find_map(|(y, line)| {
         line.iter().enumerate().find(|(_, &c)| c == b'^')
             .map(|(x, _)| (y as i32, x as i32))
     }).unwrap();
 
+    Input { guard: guard, width: width, height: height, data: lines }
+}
+
+fn search(data: &Input) -> HashSet<(i32, i32)> {
+
     let mut direction = Dir::Up;
+    let mut guard = data.guard;
     let mut visits: HashSet<(i32, i32)> = HashSet::new();
-    visits.insert(guard);
     loop {
         let next = move_pos(guard, &direction);
 
         // Off the map, done
-        if next.0 < 0 || next.1 < 0 || next.0 >= height || next.1 >= width {
+        if next.0 < 0 || next.1 < 0 || next.0 >= data.height || next.1 >= data.width {
             break;
         }
 
-        if lines[next.0 as usize][next.1 as usize] == b'#' {
+        if data.data[next.0 as usize][next.1 as usize] == b'#' {
             direction = next_dir(&direction);
             continue;
         }
@@ -63,11 +79,55 @@ pub fn part_one(input: &str) -> Option<u32> {
 
         visits.insert(guard);
     }
-    Some(visits.len() as u32)
+    visits
+}
+pub fn part_one(input: &str) -> Option<u32> {
+    let data = parse(input);
+    Some(search(&data).len() as u32 + 1)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut data = parse(input);
+
+    let paths = search(&data);
+
+    let detect_loop = |data: &Input| -> bool {
+        let mut direction = Dir::Up;
+        let mut guard = data.guard;
+        let mut visits: HashSet<((i32, i32), Dir)> = HashSet::new();
+        visits.insert((guard, direction));
+        loop {
+            let next = move_pos(guard, &direction);
+
+            // Off the map, done
+            if next.0 < 0 || next.1 < 0 || next.0 >= data.height || next.1 >= data.width {
+                return false;
+            }
+
+            if data.data[next.0 as usize][next.1 as usize] == b'#' {
+                direction = next_dir(&direction);
+                continue;
+            }
+
+            guard = next;
+            if visits.iter().contains(&(guard, direction))
+            {
+                return true;
+            }
+            visits.insert((guard, direction));
+        }
+    };
+
+    let mut count: u32 = 0;
+    for v in paths {
+        data.data[v.0 as usize][v.1 as usize] = b'#';
+        if detect_loop(&data) {
+            count = count + 1;
+        }
+        data.data[v.0 as usize][v.1 as usize] = b'.';
+    }
+
+    Some(count)
 }
 
 #[cfg(test)]
